6ec0a517a7561a96a377c04b0ce79ceef5126388
ilyazakharov <ilya_z87@mail.ru>
2022-10-26 10:31:56 +0300

    quick sorting with added memory

--- Algorithms/Sorting/QuickSort.cs
+++ Algorithms/Sorting/QuickSort.cs
@@ -2,8 +2,6 @@
 {
     public class QuickSort<T> : ISort<T> where T : IComparable
     {
-        //{}-wall
-        //(30,10,50,15)-({30},10,50,15)-(10,{30},50,15)-(10,15,{50},30)-(10,15,30,50)
         public List<T> Sort(List<T> list)
         {
             if (list == null)
@@ -11,11 +9,13 @@
                 return new List<T>();
             }
 
-            List<T> result = new(list);
-
-            return QuickSorting(result, 0, result.Count - 1);
+            //List<T> result = new(list);
+            //return QuickSorting(result, 0, result.Count - 1);
+            return QsWithNewLists(list);
         }
 
+        //{}-wall
+        //(30,10,50,15)-({30},10,50,15)-(10,{30},50,15)-(10,15,{50},30)-(10,15,30,50)
         private List<T> QuickSorting(List<T> list, int start, int end)
         {
             if (start >= end)
@@ -45,6 +45,30 @@
             list = QuickSorting(list, wall + 1, end);
 
             return list;
+        }
+
+        private List<T> QsWithNewLists(List<T> input)
+        {
+            if (input.Count < 2)
+                return input;
+
+            T pivot = input[0];
+            List<T> less = new();
+            List<T> greater = new();
+            for(int i = 1; i < input.Count; i++)
+            {
+                if (input[i].CompareTo(pivot) > 0)
+                    greater.Add(input[i]);
+                else
+                    less.Add(input[i]);
+            }
+
+            List<T> res = new List<T>(input.Count);
+            res.AddRange(QsWithNewLists(less));
+            res.Add(pivot);
+            res.AddRange(QsWithNewLists(greater));
+
+            return res;
         }
     }
 }
